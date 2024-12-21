ee6c97a738b7f98158c635bca3b4f0f93648bb9d
Aslak Hollund <aslakhol@gmail.com>
2024-12-21 17:16:17 +0100

    try to set up the flask api

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/python/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/api/python/:path*"
+            : "/api/python/",
+      },
+    ];
+  },
+};
 
 export default config;
